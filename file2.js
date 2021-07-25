e090de350fa0fc00811c5bb16d4a884ddea4efbd
akshatsankhyan22 <akshatsankhyan22@gmail.com>
2021-07-25 14:57:11 +0530

    first commit

---
+++ file2.js
@@ -0,0 +1,5 @@
+function fn(){
+    console.log("I WILL ACHIEVE MY GOALS ONE DAY");
+    return 'YES I WILL';
+}
+console.log(fn());
---
+++ file3.js
@@ -0,0 +1,4 @@
+let c= 34;
+let d = 23;
+let a = c+d;
+console.log(a);
